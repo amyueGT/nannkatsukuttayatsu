8044e22a21cb4515e90299613906396e2c15a668
amyueGT <amyuegt@gmail.com>
2025-04-01 16:01:31 +0900

    Update get-tweet-list.sh
    ##
    ## このシェルはツイッターのアーカイブファイルから日付本文を検索し日付とURLまたは某ブログサイト用のhtml形式で日付と本文をリストするシェル
    ## 基本的に自分用学習用。自分の程度の指標として見せる用
    ## ここまでの作成期間は2025年3月13日ぐらい?から2025年4月1日の18日ぐらい。作業日数は半分の9日ぐらい。
    ## 他に修正する可能性はヘルプとログを関数化と内容の整理ぐらい（？）
    ##

--- get-tweet-list.sh
+++ get-tweet-list.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/bash
 
+##
+## このシェルはツイッターのアーカイブファイルから日付本文を検索し日付とURLまたは某ブログサイト用のhtml形式で日付と本文をリストするシェル
+## 基本的に自分用学習用。自分の程度の指標として見せる用
+## ここまでの作成期間は2025年3月13日ぐらい?から2025年4月1日の18日ぐらい。作業日数は半分の9日ぐらい。
+## 他に修正する可能性はヘルプとログを関数化と内容の整理ぐらい（？）
+##
 
+output_file_name=output_list_$(date +'%Y%m%d%H%M%S')
 archive_home_dir='/archive/home/dir'
 archive_source_dir="${archive_home_dir}/archive/source/dir" #like this: twitter-yyyy-mm-dd-hash_num
 archive_data_dir=${archive_source_dir}/data
 tweets_files=tweets*.js
+account_file=account.js
 
+list_length=0
 
 ###
 ### 実行モード（テスト実行）　開始日、終了日、検索ワード（AND、OR）、アーカイブディレクトリ　dataディレクトリ　出力ファイル
 ###
-output_file_name=output_list_$(date +'%Y%m%d%H%M%S')
 date_default="1970/01/01"
 input_date_begin=$date_default
 input_date_end=""
@@ -19,12 +27,15 @@ and_search_word_length=0
 or_search_word=""
 or_search_word_length=0
 mode=""
+mode_plane_url=1
 
 for arg in $@;do
 	if [[ $arg =~ -m ]];then
 		mode=${arg#-m}
 		if [[ $mode =~ trace ]];then
 			set -o xtrace
+		elif [[ $mode =~ plane|url ]];then
+			mode=$mode_plane_url
 		fi
 		continue;
 	fi
@@ -135,23 +146,23 @@ function organaizeDateStr(){ #日時文字列と変数名を引数 "organaizeDat
 
 declare -a list
 function outputTweetList(){
-	echo call outputTweetList
+	echo run outputTweetList
 
 	list="$1"
 	#list=`echo -e $list|sort`
-	echo -e "list=$list"
-	list=$(echo -e "${list}"|sort -r|uniq)
+	echo sort list
+	list="$(echo -e "${list}"|sort -r|uniq)"
 	echo -e "list=$list"
 	list=`echo -e "$list"|awk 'BEGIN{ FS=",," } {print $2"\n"$3"\n\n";}'`
 
 
 	echo
 	echo ===head 
-	eval "echo -e \"${list}\"|head"
+	echo -e "${list}"|head
 	echo
 	echo
 	echo ===tail
-	eval "echo -e \"${list}\"|tail"
+	echo -e "${list}"|tail
 
 	echo ========updated at:$(date) >$archive_home_dir/$output_file_name
 	echo -e "${list}" >> $archive_home_dir/$output_file_name
@@ -166,6 +177,49 @@ function makePipeCommand(){
 	echo "or_search_word= $or_search_word"
 	echo "and_search_word= $and_search_word"
 
+
+	local awk_lineend_gsub=""
+	local awk_url_gsub=""
+	pipe_account_command=""
+	if [[ ! $mode == $mode_plane_url ]];then
+		pipe_account_command="|grep -e\"username\" -e\"accountDisplayName\""
+		pipe_account_command+="|awk 'BEGIN{ FS=\" : \"; } {
+			gsub(/\,|\"/,\"\");
+			if(match(\$0,\"username\")){
+				printf\"\\\"\";
+				printf\"%s\",\$2;
+				printf\",,\";
+			}else if(match(\$0,\"accountDisplayName\")){
+				printf\"%s\",\$2;
+				printf\"\\\"\";
+				printf\"%s\",RS;
+		}'"
+		echo "pipe_account_command= ${pipe_account_command}"
+
+		#"https?://[a-zA-Z0-9\041\042\043\044\045\046\047\050\051\052\053\054\055\056\057\072\073\074\075\077\100\133\134\135\137\176]*"
+
+		#\041 -> ! , #\042 -> " , #\043 -> # , #\044 -> $ , #\045 -> % , #\046 -> & , #\047 -> ' ,
+		#\050 -> ( , #\051 -> ) , #\052 -> * , #\053 -> + , #\054 -> , , #\055 -> - , #\056 -> . ,　#\057 -> / ,
+		#\072 -> : , #\073 -> ; , #\074 -> < , #\075 -> = , #\077 -> ? , 
+		#\100 -> @ , #\133 -> [ , #\134 -> \ , #\135 -> ] , #\137 -> _ , 
+		#\176 -> ~
+
+		#awk_gsub_begin=" FS=\",,\"; url_reg=https?://[a-zA-Z0-9*\047\072();:@&=+$,/\?%25%22%23~-_\[!\].]*; "
+		awk_gsub_begin='FS=",,"; url_reg="https?://[a-zA-Z0-9\041\042\043\044\045\046\047\050\051\052\053\054\055\056\057\072\073\075\077\100\133\134\135\137\176]*";';
+		awk_gsub_chars="gsub(/%/,\"\\045\");"
+		awk_gsub_lineend="gsub(/\\\n/,\"<br>\");${awk_gsub_chars}"
+		
+		#awk_gsub_gsub="gsub(/(https?[[:alnum:][:punct:]]*)/,\"\\<a href=\"\\1\">\\1\\</a\\>\");"
+		#awk_gsub_gsub="gsub(/\\n/,\"<br>\");match(\$0,\"https?://[^<[:space:][^ぁ-んァ-ヴ０-９Ａ-Ｚ]]*\"); url=substr(\$0,RSTART,RLENGTH);printf(\"<a href=\"%s\">%s</a>\",url,url);"
+		awk_gsub_url="if(match(\$0,url_reg)){ \
+			url=substr(\$0,RSTART,RLENGTH); \
+			html=\"<a href=\\\"\"url\"\\\">\"url\"</a>\"; \
+			gsub(url,html,\$0);}"
+			#gsub(/url_reg/,html,\$3);"
+
+		pipe_awk_gsub_to_html="|awk 'BEGIN{ ${awk_gsub_begin} } { ${awk_gsub_lineend}${awk_gsub_url} print }'"
+		echo pipe_awk_gsub_to_html= $pipe_awk_gsub_to_html
+	fi
 
 	pipe_cat_grep="|grep -E --color=no -ecreated_at -e'^\s{6}\"id_str\"' -efull_text"
 	pipe_grep_awk="|awk 'BEGIN{ FS=\" : \"; } {
@@ -199,7 +253,17 @@ function makePipeCommand(){
 }
 
 
-list_length=0
+embed_text='<blockquote class=\"twitter-tweet\" data-conversation=\"none\" data-lang=\"ja\"><p lang=\"ja\" dir=\"ltr\">%s</p>&mdash; %s (%s) <a href=\"%s?ref_src=twsrc\0455Etfw\">%s</a></blockquote><p><script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script></p>'
+function makeEmbedText(){
+	tweet_date=$(date -d@$created_date $embed_tweet_date_format)
+	embed_text=$(printf $embed_text $account_name $tweet_text)
+}
+
+
+#local twitter_date_format='+%a %b %d %H:%M:%S %z %Y'
+output_date_format='+%Y/%m/%d %H:%M:%S'
+embed_tweet_date_format='+%Y年%m月%d日'
+
 function getList(){
 	echo call getList
 
@@ -210,16 +274,13 @@ function getList(){
 	repeat=$2
 	if (( repeat > 0 ));then rt='>>'; fi
 
-	#local twitter_date_format='+%a %b %d %H:%M:%S %z %Y'
-	local output_date_format='+%Y/%m/%d %H:%M:%S'
-
 
 	echo input_date_begin=$input_date_begin input_date_end=$input_date_end
 	if (( input_date_begin>input_date_end ));then echo end date is before than begin date. end shell ;return; fi
 	
 
 	local created_date=""
-	local tweet_url
+	local tweet_url=""
 	local id=""
 
 	command_file_cat="cat ${file}"
@@ -277,17 +338,26 @@ function getList(){
 			continue;
 		fi
 		echo "" #改行を差し込むためのエコー
-		tweet_url=`echo ${line}|awk 'BEGIN{FS=",,"} {print $1}'`
+
+		additional_txt=""
 		tweet_text=`echo ${line}|awk 'BEGIN{FS=",,"} {print $3}'`
+		tweet_url=`echo ${line}|awk 'BEGIN{FS=",,"} {print $1}'`
+		if [[ ! $mode == $mode_plane_url ]];then
+			echo "eval echo '${tweet_text}'${pipe_awk_gsub_to_html}"
+			tweet_text=`eval "echo '${tweet_text}'${pipe_awk_gsub_to_html}"`
+			tweet_date=$(date -d@$created_date $embed_tweet_date_format)
+			tweet_url=$(echo ${line}|awk 'BEGIN{FS=",,"} {print $1}')
 
-		additional=""
-		if [[ $tweet_text =~ 休み ]];then
-			additional=::発言休み
+			echo "printf \"${embed_text}\" \"${tweet_text}\" \"${account_name}\" \"${account_id}\" \"${tweet_url}\" \"${tweet_date}\""
+			tweet_url="$(printf "${embed_text}" "${tweet_text}" "${account_name}" "${account_id}" "${tweet_url}" "${tweet_date}")"
 		fi
-		#echo ===== $created_date $tweet_url =====
-		line="$created_date,,発言日:$(date -d"@$created_date" "$output_date_format")$additional,,$tweet_url\n"
-		echo " --> rewrited: $line"
+		if [[ $tweet_text =~ 休み ]];then
+			additional_txt=" &mdash; 発言休み"
+		fi
+		date_text="<p>発言日:$(date -d"@$created_date" "$output_date_format")$additional_txt</p>"
 
+		line="${created_date},,${date_text},,${tweet_url}\n"
+		echo " --> rewrited: $line"
 		list+="$line"
 		((list_length++))
 
@@ -303,17 +373,21 @@ function getTweetFiles(){
 	arr=(`find $archive_data_dir/$tweets_files|awk 'ORS=" " {print}'`);
 	eval "$1=(${arr[@]})"
 }
-
+function getAccountFile(){
+	echo run getAccountFile
+	eval "$1=(`find $archive_data_dir/$account_file`);"
+}
 
 echo "\$1=$1" "\$2=$2" \$3="$3" \$4="$4" #"\$5=$5"
 
 
-
+declare -a files
+acnt_file=""
+getTweetFiles files
+getAccountFile acnt_file
 repeat=0
 grep_result=""
 
-#local input_date_begin=`LANG=ja_US.UTF-8 date -u -d"$2" +'%a %b %d %H:%M:%S %z %Y'`
-#local input_date_end=`LANG=ja_US.UTF-8 date -u -d"$3" +'%a %b %d %H:%M:%S %z %Y'`
 if [[ -z $input_date_end ]];then input_date_end="$input_date_begin"; fi
 if [[ ! $input_date_begin =~ ^[0-9]$ ]];then
 	echo input_date_begin=$input_date_begin input_date_end=$input_date_end
@@ -327,9 +401,12 @@ if [[ ! $input_date_begin =~ ^[0-9]$ ]];then
 fi
 
 makePipeCommand
+echo "cat \"${archive_data_dir}/${account_file}\"|grep username |awk 'BEGIN{ FS=\" : \" }{gsub(/\,|\"/,\"\"); printf(\$2) }'"
+account_id=$(eval "cat \"${archive_data_dir}/${account_file}\"|grep username |awk 'BEGIN{ FS=\" : \" }{gsub(/\,|\"/,\"\"); print(\$2) }'")
+echo eval "cat \"${archive_data_dir}/${account_file}\"|grep accountDisplayName |awk 'BEGIN{ FS=\" : \" }{gsub(/\,|\"/,\"\"); print(\$2) }'"
+account_name=$(eval "cat \"${archive_data_dir}/${account_file}\"|grep accountDisplayName |awk 'BEGIN{ FS=\" : \" }{gsub(/\,|\"/,\"\"); print(\$2) }'")
+echo account_id=$account_id  account_name=$account_name
 
-declare -a files
-getTweetFiles files
 for file in ${files[@]};
 do
 	getList "$file" $repeat
@@ -340,10 +417,16 @@ do
 	echo or_search_result:liines ${#or_search_result}
 	echo and_search_result:liines ${#and_search_result}
 
+	grep_result+=$(echo ${or_search_result}|sort -r|awk "BEGIN{ FS=\",,\" }{printf\"%s,,%s\n\n\",\$2,\$1}")
+	grep_result+=$(echo ${and_search_result}|sort -r|awk "BEGIN{ FS=\",,\" }{printf\"%s,,%s\n\n\",\$2,\$1}")
 done
 echo loop end
 
 outputTweetList "${list}" #"${list[@]}"
+
+
+echo -e `date`=====================search_result > $archive_home_dir/greped_tweets
+echo -e "${grep_result}" >> $archive_home_dir/greped_tweets
 
 echo ==========================================
 echo
